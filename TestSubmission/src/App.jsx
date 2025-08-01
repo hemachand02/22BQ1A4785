db1fd9b045f19be980be2bb3200f2647488807f6
hemachand02 <129775421+hemachand02@users.noreply.github.com>
2025-08-01 12:50:01 +0530

    Update App.jsx

--- TestSubmission/src/App.jsx
+++ TestSubmission/src/App.jsx
@@ -1,1 +1,18 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ShortenerForm from './components/ShortenerForm';
+import StatisticsPage from './components/StatisticsPage';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ShortenerForm />} />
+        <Route path="/stats" element={<StatisticsPage />} />
+        <Route path="/:shortcode" element={<RedirectHandler />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
 
